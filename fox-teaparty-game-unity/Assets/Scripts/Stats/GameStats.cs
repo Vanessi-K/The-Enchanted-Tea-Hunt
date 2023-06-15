ccdb5eafa0b3262c961635a727dcedacd9aae64a
Vanessi-K <vanessa.koeck.26@outlook.com>
2023-06-15 08:04:20 +0200

    Added timer

--- fox-teaparty-game-unity/Assets/Scripts/Stats/GameStats.cs
+++ fox-teaparty-game-unity/Assets/Scripts/Stats/GameStats.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -18,8 +19,10 @@ public enum CollectionState
 
 public class GameStats : MonoBehaviour
 {
+    [SerializeField] private TMP_Text timerText;
     public static GameStats Instance;
     private Backpack _backpack;
+    private float _timer;
 
     public Backpack Backpack
     {
@@ -27,18 +30,27 @@ public class GameStats : MonoBehaviour
         private set { _backpack = value; }
     }
     
-    void Awake()
+    private void Awake()
     {
         if (Instance == null)
         {
             GameStats.Instance = this;
             _backpack = GetComponent<Backpack>();
+            _timer = 0;
             DontDestroyOnLoad(gameObject); 
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+    
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(_timer / 60);
+        int seconds = Mathf.FloorToInt( _timer - minutes * 60);
+        timerText.text = $"{minutes:00}:{seconds:00}";
     }
 
     public void LoadSceneCollectibles(string sceneLayer)
